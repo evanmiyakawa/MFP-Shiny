9e9cbafa0a83b7264e8e4890bc6fc5152c2d4171
evanmiyakawa <evanmiyakawa@gmail.com>
2019-07-22 15:28:48 -0500

    initialization

---
+++ app.R
@@ -0,0 +1,15 @@
+# R Shiny
+
+# https://www.shinyproxy.io/deploying-apps/
+# https://www.r-bloggers.com/deploying-an-r-shiny-app-with-docker/
+# http://shiny.rstudio.com/articles/shinyapps.html
+# https://www.shinyapps.io/admin/#/dashboard
+
+
+library(tidyverse)
+library(shiny)
+library(DT)
+
+
+
+runApp("Test App")
